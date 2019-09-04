a8fb994c53941269b8dc48248c69cafeeeaa366e
surpassm <39703664+surpassm@users.noreply.github.com>
2019-09-04 14:14:00 +0800

    Update SecurityController.java

--- src/main/java/com/github/surpassm/security/controller/SecurityController.java
+++ src/main/java/com/github/surpassm/security/controller/SecurityController.java
@@ -68,10 +68,12 @@ public class SecurityController {
 			}
 		}
 		Cookie[] cookies = request.getCookies();
-		for (Cookie cookie : cookies) {
-			cookie.setMaxAge(0);
-			cookie.setPath("/");
-			response.addCookie(cookie);
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				cookie.setMaxAge(0);
+				cookie.setPath("/");
+				response.addCookie(cookie);
+			}
 		}
 		//清空当前缓存
 		HttpSession session = request.getSession();
