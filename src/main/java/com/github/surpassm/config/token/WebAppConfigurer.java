6ab8dc3f10c824d3bb60e8ee08fcc7583727ce58
surpassm <lustres@sina.com>
2019-09-06 17:53:04 +0800

    新增自定义注解

--- src/main/java/com/github/surpassm/config/token/WebAppConfigurer.java
+++ src/main/java/com/github/surpassm/config/token/WebAppConfigurer.java
@@ -1,14 +1,23 @@
 package com.github.surpassm.config.token;
 
 import com.github.surpassm.security.properties.SecurityProperties;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import javax.annotation.Resource;
 import java.io.File;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author mc
@@ -32,6 +41,7 @@ public class WebAppConfigurer implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
+//		getEnableExemptionAuth(securityProperties);
 		InterceptorRegistration interceptorRegistration = registry.addInterceptor(tokenInterceptor);
 		String[] noVerify = securityProperties.getNoVerify();
 		if (noVerify != null && noVerify.length != 0){
