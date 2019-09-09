86a7f6b1dadf96b3c0cc6da23b5baf48b71e2f36
surpassm <lustres@sina.com>
2019-09-09 09:13:54 +0800

    新增自定义注解

--- src/main/java/com/github/surpassm/config/token/WebAppConfigurer.java
+++ src/main/java/com/github/surpassm/config/token/WebAppConfigurer.java
@@ -1,14 +1,24 @@
 package com.github.surpassm.config.token;
 
+import com.github.surpassm.config.annotation.EnableExemptionAuth;
 import com.github.surpassm.security.properties.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import javax.annotation.Resource;
 import java.io.File;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author mc
@@ -17,6 +27,7 @@ import java.util.List;
  * Description
  */
 @Configuration
+@Order(Integer.MAX_VALUE)
 public class WebAppConfigurer implements WebMvcConfigurer {
     @Resource
     private TokenMethodArgumentResolver tokenMethodArgumentResolver;
@@ -24,6 +35,8 @@ public class WebAppConfigurer implements WebMvcConfigurer {
 	private TokenInterceptor tokenInterceptor;
     @Resource
 	private SecurityProperties securityProperties;
+	@Resource
+	private RequestMappingHandlerMapping mapping;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
@@ -58,4 +71,37 @@ public class WebAppConfigurer implements WebMvcConfigurer {
 				.allowCredentials(true).maxAge(36000);
 	}
 
+	/*private void getEnableExemptionAuth(SecurityProperties securityProperties){
+		//新建一个数组
+		List<String> exemptionUrl = new ArrayList<>();
+		//新增默认排除项
+		exemptionUrl.add("/swagger-");
+		exemptionUrl.add("/images/");
+		exemptionUrl.add("/webjars/");
+		exemptionUrl.add("/v2/api-docs");
+		exemptionUrl.add("/swagger-resources/configuration/**");
+		exemptionUrl.add("/websocket/socketServer.ws");
+		exemptionUrl.add("/sockjs/socketServer.ws");
+		Map<RequestMappingInfo, HandlerMethod> map = mapping.getHandlerMethods();
+		for (Map.Entry<RequestMappingInfo, HandlerMethod> m : map.entrySet()) {
+			RequestMappingInfo info = m.getKey();
+			HandlerMethod method = m.getValue();
+			Method method1 = method.getMethod();
+			Annotation[] annotations = method1.getAnnotations();
+			boolean tag = false;
+			for (Annotation annotation : annotations) {
+				if (annotation.annotationType().isAnnotationPresent(EnableExemptionAuth.class)) {
+					tag = true;
+				}
+			}
+			if (tag) {
+				PatternsRequestCondition p = info.getPatternsCondition();
+				for (String url : p.getPatterns()) {
+					exemptionUrl.add(url + "**");
+				}
+			}
+		}
+		Object[] objects = exemptionUrl.toArray();
+		securityProperties.setNoVerify((String[]) objects);
+	}*/
 }
